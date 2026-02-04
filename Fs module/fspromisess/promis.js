1584554b71e3777bdcfe1353cdd4151d60a0c611
Ashutosh Kumar <ashutoskumar53009@gmail.com>
2026-02-05 00:13:47 +0530

    fspromisess commot

--- Fs module/fspromisess/promis.js
+++ Fs module/fspromisess/promis.js
@@ -5,7 +5,35 @@ const path = require('path');
 const fileName = "fspromise.txt";
 const filePath= path.join(__dirname,fileName);
 const filepath1= __dirname;
-fs.promises
-.readdir(filepath1)
-.then((data) =>console.log(data))
-.catch((err) =>console.log(err));
+// fs.promises
+// .readdir(filepath1)
+// .then((data) =>console.log(data))
+// .catch((err) =>console.log(err));
+
+
+// Cretae or Write file 
+
+// fs.promises.writeFile(filePath,"This is the initaial data", "utf-8")
+// .then(console.log("File created successfully"))
+// .catch((err)  => console.log(err));
+
+// ReadFile 
+
+
+// fs.promises.readFile(filePath, "utf-8")
+// .then((data) =>console.log(data))
+// .catch((err)  => console.log("Error encoding file",err));
+
+// Updte file or appendfile 
+
+// fs.promises.appendFile(filePath," \nThis is the updated  data", "utf-8")
+// .then(console.log("File updated  successfully"))
+// .catch((err)  => console.log(err));
+
+
+// File deleted or unlink 
+
+// fs.promises.unlink(filePath)
+// .then(console.log("File Deleted  successfully"))
+// .catch((err)  => console.log( "Error deleting file",err));
+
