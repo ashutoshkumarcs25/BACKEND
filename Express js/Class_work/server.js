d1a638cbed5df4445703a3cc0e89288591cf9e7b
ashutoshkumarcs25 <your-email@example.com>
2026-03-20 21:28:06 +0530

    Ecpress js commit

--- Express js/Class_work/server.js
+++ Express js/Class_work/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({
-        name: "hit",
-        age: 19
-    });
+app.get("/sum", (req, res) => {
+    console.log(req.query);
+    const ans = parseInt(req.query.a)+ parseInt(req.query.b);
+    res.send(ans.toString());
 });
 
+
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3000" );
 });
