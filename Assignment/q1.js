40700c481c1a55303855c5ac50e9826bf6c56eef
ashutoshkumarcs25 <your-email@example.com>
2026-03-22 01:30:38 +0530

    Assignment commit

---
+++ Assignment/q1.js
@@ -0,0 +1,16 @@
+
+// for loop
+// const arr = [10, 20, 30, 40];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+//for off loop
+
+const arr = [10, 20, 30, 40];
+
+for (let value of arr) {
+    console.log(value);
+}
