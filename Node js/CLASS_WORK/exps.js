80cb18204950cf324def5f07df88b16cc6d9fdd4
Ashutosh kumar <ashutosh.kumar_cs25@gla.ac.in>
2026-04-12 00:43:23 +0530

    exps.js commit

---
+++ Node js/CLASS_WORK/exps.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  if (req.method === "POST") {
+    let body = "";
+    
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      fs.writeFile("data.txt", body, "utf-8", () => {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Done");
+      });
+    });
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
